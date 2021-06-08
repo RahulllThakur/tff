cd255d230ed7ab2b5e2fea7f7394c66819353f00
RahulllThakur <36402599+RahulllThakur@users.noreply.github.com>
2021-06-08 13:58:10 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  default = "AKIA5KOY4NEOEZ4VCKGU"
-  }
-variable "secret_key" {
-  default = "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"
-  }
-variable "region" {
-  default = "ap-south-1"
-  }
